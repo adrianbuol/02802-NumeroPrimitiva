0dfea8631cf0539029cc6fa67809b86e617c5756
adria <adria@192.168.1.5>
2019-10-29 18:48:59 +0100

    :tada: Commit inicial

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,38 @@
+/* 
+ * Copyright 2019 Adrian Bueno Olmedo <adrian.bueno.alum@iescamp.es>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Adrian Bueno Olmedo <adrian.bueno.alum@iescamp.es>
+ */
+public class Main {
+       
+        public static final Scanner SCN = 
+	  new Scanner(System.in, "Windows-1252")
+	  .useLocale(Locale.ENGLISH).useDelimiter("\\s+");
+        
+        public static void main(String[] args){
+            System.out.printf("Inserte su Numero favorito.....:");
+            int NumeroPrimitiva = SCN.nextInt();
+            
+            System.out.printf("Numero primitiva ..............:%d%n", NumeroPrimitiva);
+}     
+       
+}
